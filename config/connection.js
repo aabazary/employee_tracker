36f6e85b072121a693a08cc451d289fa629258a1
aabazary <aabazary@gmail.com>
2021-08-29 19:03:58 -0700

    corrected dotenv file

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,15 @@
-const Sequelize = require('sequelize');
+const mysql = require('mysql2');
 
 // Enable access to .env variables
 require('dotenv').config();
 
 // Use environment variables to connect to database
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  }
-);
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: process.env.DB_PASSWORD,
+  database: 'employee_tracker_db'
+});
 
-module.exports = sequelize;
+module.exports = connection;
--- server.js
+++ server.js
@@ -2,18 +2,11 @@ const inquirer = require("inquirer")
 const mysql = require("mysql2")
 const ct = require('console.table');
 const express = require('express');
-
+const db = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'grande123',
-    database: 'employee_tracker_db',
-  },
-  console.log(`Connected to the employee_tracker database.`)
-);
+
 
 app.use(express.json());
 app.use(express.urlencoded({
