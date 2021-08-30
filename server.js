54ddba1644fc4c0abfbc3c7e092e15e9b8a69bf5
aabazary <aabazary@gmail.com>
2021-08-29 21:21:30 -0700

    updated more functionality, may need to change role and manager logic to allow for lists

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const inquirer = require("inquirer")
-const mysql = require("mysql2")
 const ct = require('console.table');
 const express = require('express');
 const db = require('./config/connection');
@@ -86,9 +85,97 @@ function (err, results) {
   initPrompt()
 })};
 
-function addDepartment(){};
-function addRole(){};
-function addEmployee(){};
+function addDepartment(){
+  inquirer.prompt([
+    {
+      name: "name",
+      type: "input",
+      message: "Name of new department"
+    }
+]).then(function(res) {
+    db.query( "INSERT INTO department SET ? ",
+        {
+          name: res.name
+        },
+        function(err) {
+            if (err) throw err
+            console.log(res.name, "added as a Department");
+            initPrompt();
+        }
+    )
+})
+};
+function addRole(){
+  inquirer.prompt([
+    {
+      name: "name",
+      type: "input",
+      message: "Name of new role"
+    },
+    {
+      name: "salary",
+      type: "input",
+      message: "Salary of the role"
+    },
+    {
+      name: "department",
+      type: "input",
+      message: "Department to assign new role"
+    }
+]).then(function(res) {
+    db.query( "INSERT INTO role SET ? ",
+        {
+          title: res.name,
+          salary: res.salary,
+          department_id: res.department,
+        },
+        function(err) {
+            if (err) throw err
+            console.log(res.name, "added as a new Role");
+            initPrompt();
+        }
+    )
+})
+};
+function addEmployee(){
+  inquirer.prompt([
+    {
+      name: "firstname",
+      type: "input",
+      message: "First name "
+    },
+    {
+      name: "lastname",
+      type: "input",
+      message: "Last name "
+    },
+    {
+      name: "role",
+      type: "input",
+      message: "What is their role? ",
+    },
+    {
+      name: "manager",
+      type: "input",
+      message: "Who is the Manager? ",
+    }
+  ]).then(function (res) {
+    db.query("INSERT INTO employee SET ?", 
+    {
+        first_name: res.firstname,
+        last_name: res.lastname,
+        role_id: res.role,
+        manager_id: res.manager
+        
+        
+    }, function(err){
+        if (err) throw err
+        console.table(res.firstname,"added as a new Employee")
+        initPrompt()
+    })
+
+})
+};
 function addEmployeeRole(){};
 
 initPrompt()
