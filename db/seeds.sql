2acc4bb0dcc6eb45eac5b09aa71b03576b56738f
aabazary <aabazary@gmail.com>
2021-08-29 18:24:24 -0700

    updated file structure, created initital prompt, added seeds, working on functionality of prompts

---
+++ db/seeds.sql
@@ -0,0 +1,79 @@
+INSERT INTO department (name)
+VALUE ("Engineering");
+INSERT INTO department (name)
+VALUE ("Manufacturing");
+INSERT INTO department (name)
+VALUE ("Research & Development");
+INSERT INTO department (name)
+VALUE ("Marketing");
+INSERT INTO department (name)
+VALUE ("Legal");
+INSERT INTO department (name)
+VALUE ("Sales");
+
+
+INSERT INTO role (title, salary, department_id)
+VALUE ("Engineer Manager", 500000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Chief Engineer", 400000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lead Engineer", 300000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Senior Engineer", 200000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Junior Engineer", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Manufacturing Manager", 200000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Manufacturing Foreman", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Manufacturer", 100000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lead Developer", 200000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Researcher", 100000, 3;
+INSERT INTO role (title, salary, department_id)
+VALUE ("Marketing Manager", 200000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Marketing Associate", 100000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lawyer", 1000000, 5);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Sales Manager", 200000, 6);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Sales Associate", 100000, 6);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Archimedes", "of Syracuse", 1 , null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Galileo", "Galilei", 2 , null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Charles", "Darwin", 3 , 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Issac", "Newton", 4 , 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Stephen", "Hawking", 5 , 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Thomas", "Edison", 6 , 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Henry", "Ford", 7 , 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("John", "Smith", 8 , 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Steve", "Jobs", 9 , 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Steve", "Wozniak", 10 , 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Plato", "Aristocles", 11 , 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Jordan", "Belfort", 12 , 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Pope", "Francis", 14 , null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Teresa", "of Calcutta", 6 , null);
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,13 +1,93 @@
 const inquirer = require("inquirer")
 const mysql = require("mysql2")
 const ct = require('console.table');
+const sequelize = require('./config/connection');
+const express = require('express');
 
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'employee_tracker_db',
-    },
-    console.log(`Connected to the employee_tracker database.`)
-  );
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'employee_tracker_db',
+//   },
+//   console.log(`Connected to the employee_tracker database.`)
+// );
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+
+function initPrompt() {
+  inquirer.prompt([{
+    type: "list",
+    message: "Make a Selection:",
+    name: "choice",
+    choices: [
+      "View All Departments?",
+      "View All Roles?",
+      "View all Employees",
+      "Add Department?",
+      "Add Role?",
+      "Add Employee?",
+      "Update an Employee Role?",
+    ]
+  }]).then(function (event) {
+    switch (event.choice) {
+      case "View All Departments?":
+        viewAllDepartments();
+        break;
+
+      case "View All Roles?":
+        viewAllRoles();
+        break;
+      case "View all Employees":
+        viewAllEmployees();
+        break;
+
+      case "Add Department?":
+        addDepartment();
+        break;
+
+      case "Add Role?":
+        addRole();
+        break;
+
+      case "Add Employee?":
+        addEmployee();
+        break;
+
+      case "Update an Employee Role?":
+        addEmployeeRole();
+        break;
+
+    }
+  })
+}
+function viewAllDepartments(){
+//  SELECT * FROM department;
+  sequelize.query("SELECT * FROM department",
+  function (err, results) {
+    if (err) throw err
+    console.table(results)
+    initPrompt()
+})};
+
+function viewAllRoles(){
+// SELECT * FROM role;
+
+};
+function viewAllEmployees(){
+//SELECT * FROM employee;
+
+};
+function addDepartment(){};
+function addRole(){};
+function addEmployee(){};
+function addEmployeeRole(){};
+
+initPrompt()
