93c97f266f48d18d2dc90ac7065f7d6f060664d3
aabazary <aabazary@gmail.com>
2021-08-31 14:49:55 -0700

    attempting to change from manager_id to the name

--- db/seeds.sql
+++ db/seeds.sql
@@ -78,10 +78,11 @@ VALUE ("Teresa", "of Calcutta", 14 , null);
 -- FROM department
 -- INNER JOIN role ON role.department_id=department.id;
 
--- SELECT employee.id, employee.first_name, employee.last_name, role.title,department.name, role.salary, employee.manager_id
+-- SELECT employee.id, employee.first_name, employee.last_name, role.title,department.name, role.salary, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager 
 -- FROM employee
 -- INNER JOIN role ON employee.role_id=role.id
--- INNER JOIN department ON role.department_id=department.id;
+-- INNER JOIN department ON role.department_id=department.id
+-- INNER JOIN employee e ON employee.manager_id =e.id;
 
 -- SELECT employee.id, CONCAT(employee.first_name," ", employee.last_name), role.title
 -- FROM employee
