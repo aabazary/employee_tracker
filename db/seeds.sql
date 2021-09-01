b2a0bdf1bdab63df352552f1f44e2e2d5003f74c
aabazary <aabazary@gmail.com>
2021-08-31 18:34:43 -0700

    updated table names

--- db/seeds.sql
+++ db/seeds.sql
@@ -78,11 +78,11 @@ VALUE ("Teresa", "of Calcutta", 14 , null);
 -- FROM department
 -- INNER JOIN role ON role.department_id=department.id;
 
-SELECT employee.id, employee.first_name, employee.last_name, role.title,department.name, role.salary, CONCAT(manager.first_name, ' ' ,manager.last_name) AS Manager 
-FROM employee
-INNER JOIN role ON employee.role_id=role.id
-INNER JOIN department ON role.department_id=department.id
-LEFT OUTER JOIN employee manager ON employee.manager_id =manager.id; 
+-- SELECT employee.id, employee.first_name, employee.last_name, role.title,department.name, role.salary, CONCAT(manager.first_name, ' ' ,manager.last_name) AS Manager 
+-- FROM employee
+-- INNER JOIN role ON employee.role_id=role.id
+-- INNER JOIN department ON role.department_id=department.id
+-- LEFT OUTER JOIN employee manager ON employee.manager_id =manager.id; 
 
 -- SELECT employee.id, CONCAT(employee.first_name," ", employee.last_name), role.title
 -- FROM employee
